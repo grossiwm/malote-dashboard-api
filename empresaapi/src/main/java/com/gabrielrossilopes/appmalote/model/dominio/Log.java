ecca27963ff84a409fc36f9f99038a096627ad68
Gabriel <grossiwm@gmail.com>
2022-03-21 20:32:49 -0300

    optimize imports

--- empresaapi/src/main/java/com/gabrielrossilopes/appmalote/model/dominio/Log.java
+++ empresaapi/src/main/java/com/gabrielrossilopes/appmalote/model/dominio/Log.java
@@ -1,12 +1,7 @@
 package com.gabrielrossilopes.appmalote.model.dominio;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "TLog")
--- usuarioapi/src/main/java/com/gabrielrossilopes/appmalote/controller/api/UsuarioController.java
+++ usuarioapi/src/main/java/com/gabrielrossilopes/appmalote/controller/api/UsuarioController.java
@@ -1,15 +1,11 @@
 package com.gabrielrossilopes.appmalote.controller.api;
 
-import com.gabrielrossilopes.appmalote.dto.UsuarioDTO;
-import com.gabrielrossilopes.appmalote.model.dominio.Usuario;
 import com.gabrielrossilopes.appmalote.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/usuario")
